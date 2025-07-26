1da83534006cb48af0cf875b16cf2328864b702a
Samuel Jenks <mail@samueljenks.me>
2025-07-26 13:07:54 +1200

    rm: maybe unnecessary

--- scripts/SignalBus.cs
+++ scripts/SignalBus.cs
@@ -3,7 +3,6 @@ using System;
 
 public partial class SignalBus : Node
 {
-    [Signal]
     public delegate void Del_LevelExpandedEventHandler();
     public event Del_LevelExpandedEventHandler LevelExpanded;
 

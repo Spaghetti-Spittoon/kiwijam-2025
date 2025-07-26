2eda201c43705f9cd451b52e81d5a05a819c778b
Samuel Jenks <mail@samueljenks.me>
2025-07-26 12:24:59 +1200

    feat: basic level

--- scripts/StartButton.cs
+++ scripts/StartButton.cs
@@ -3,4 +3,21 @@ using System;
 
 public partial class StartButton : Button
 {
+	PackedScene level;
+
+	public override void _Ready()
+	{
+		level = GD.Load<PackedScene>("res://scenes/Level.tscn");
+		ButtonUp += OnClick;
+	}
+
+	public void OnClick()
+	{
+		var instance = level.Instantiate<Level>();
+
+		var parent = this.GetParent();
+		var cast2D = (CanvasLayer)parent;
+		cast2D.AddSibling(instance);
+		cast2D.Hide();
+	}
 }

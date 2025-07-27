8b8da709f9cd3420a683315790d922848440ffdf
Paulo Mendes de Lima <pbenety@gmail.com>
2025-07-27 14:11:09 +1200

    Generate new words

--- scripts/Level.cs
+++ scripts/Level.cs
@@ -1,9 +1,11 @@
 using Godot;
-using System;
 
 public partial class Level : Node2D
 {
 	private PackedScene enemyScene;
+	private PackedScene wordScene;
+
+	private string[] words;
 
 	TileMapLayer mapLayer;
 	SignalBus bus;
@@ -13,12 +15,21 @@ public partial class Level : Node2D
 	public override void _Ready()
 	{
 		enemyScene = ResourceLoader.Load("res://scenes/enemy.tscn") as PackedScene;
+		wordScene = ResourceLoader.Load("res://scenes/word.tscn") as PackedScene;
 		mapLayer = GetNode<TileMapLayer>("TileMapLayer");
 		bus = GetNode<SignalBus>("/root/SignalBus");
 		button = GetNode<Button>("TestIncreaseButton");
 		grid = GetNode<Grid>("/root/Grid");
 		button.ButtonUp += OnLevelExpanded;
 		bus.LevelExpanded += OnLevelExpanded;
+
+		var wordsFile = FileAccess.Open("res://assets/phone_words.json", FileAccess.ModeFlags.Read);
+		var wordsString = wordsFile.GetAsText();
+		var parsedJson = Json.ParseString(wordsString);
+		var jsonAsDictionary = parsedJson.AsGodotDictionary();
+		words = (string[]) jsonAsDictionary["phone_words"];
+
+		wordsFile.Close();
 	}
 
 	void OnLevelExpanded()
@@ -62,5 +73,20 @@ public partial class Level : Node2D
 		enemy.Position = enemySpawnLocation.Position;
 
 		AddChild(enemy);
+	}
+
+	private void OnWordTimerTimeout()
+	{
+		Word word = wordScene.Instantiate<Word>();
+
+		var phoneLocation = GetNode<Node2D>("Phone");
+
+		string nextWord = words[GD.Randi() % words.Length];
+
+		word.SetText(nextWord);
+
+		word.Position = phoneLocation.Position;
+
+		AddChild(word);
 	}
 }
